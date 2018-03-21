72e1c4578ff1bfae997fe19ad8e2ca1a1b2b0f35
Wenjie <traxiaojie@gmail.com>
2018-03-20 21:01:27 -0700

    Finished integer palindrome check function, all tests passed

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,26 @@
-# Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
+# Thought process:
+# step 1: reverse the integer
+# step 2: compare the reversed integer to the given number
+# Time Complexity: O(m)
+# Space complexity: O(n)
+
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number.class != Integer || number < 0
+
+  # create a varibale to store the result number
+  result = 0
+  prev_result = 0
+  copy_num = number
+
+  while copy_num != 0
+    current_digit = copy_num % 10
+    result = result * 10 + current_digit
+
+    # This is to handle overflow
+    return false if ((result - current_digit) / 10) != prev_result
+
+    prev_result = result
+    copy_num = copy_num / 10
+  end
+  return result == number
 end
